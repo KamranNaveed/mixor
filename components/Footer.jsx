1f19e2382a8010559246d94ec52baf5b6e74cab6
KamranNaveed <kamran.naveedsyed@gmail.com>
2022-12-25 13:33:44 -0500

    added homepage, product page and cart

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai"
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 Mixor Headphones All Rights Reserved</p>
+      <p className="icons"><AiFillInstagram/><AiOutlineTwitter/></p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ context/StateContext.js
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({children}) => {
+    const [showCart, setShowCart] = useState(false)
+    const [cartItems, setCartItems] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0)
+    const [totalQuantities, setTotalQuantities] = useState(0)
+    const [quantity, setQuantity] = useState(1)
+
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems?.find(item=>item._id===product._id)
+        setTotalPrice = (prevTotalPrice => prevTotalPrice + product.price * quantity)
+        setTotalQuantities(prevTotalQuantities => prevTotalQuantities + quantity)
+
+
+        if(checkProductInCart){
+            const updatedCartItems = cartItems.map((cartProduct)=>{
+                if (cartProduct._id === product._id) return {...cartProduct, quantity: cartProduct.quantity+quantity}
+            })
+            setCartItems(updatedCartItems)
+        } else {
+            product.quantity = quantity
+            setCartItems([...cartItems, {...product}])
+        }
+        toast.success(`${quantity} ${product.name} added to the cart`)
+    }
+
+    const increaseQuantity = ()=>{
+        setQuantity((prevQuantity)=>prevQuantity+1)
+    }
+
+    const decreaseQuantity = ()=>{
+        setQuantity((prevQuantity)=>{return prevQuantity < 1 ? 1 : prevQuantity-1})
+    }
+
+    return(<Context.Provider value={{
+        showCart, cartItems, totalPrice, totalQuantities, quantity, increaseQuantity, decreaseQuantity, onAdd, setShowCart
+    }}>
+        {children}
+    </Context.Provider>)
+}
+
+export const useStateContext = ()=>useContext(Context)
